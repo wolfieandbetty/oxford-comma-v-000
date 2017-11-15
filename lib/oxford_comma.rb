d2e8f6ab6143a0fa8a63e6df04bf499064f258be
Cat Norris <cannenorris@yahoo.com>
2017-11-15 22:50:25 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
-array[-1]="and #{array[-1]}"
-array = array.join(", ")
+if array.size == 1
+  array
+elsif array.size >2
+  array = array.join(", ")
+  array[-1]="and #{array[-1]}"
 
 array
 end
