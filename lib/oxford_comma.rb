a1d6708a4e2864edd825d8af6df6b70cb97de768
Cat Norris <cannenorris@yahoo.com>
2017-11-16 00:30:09 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,10 @@
 def oxford_comma(array)
-if array.size == 1
-  array=array[0]
-elsif array.size ==2
-  array = array.join(", ")
-  array[-1]="and #{array[-1]}"
+  
+if array.length ==2
+  return "#{array[0]} and #{{array[1]}"
+elsif 2 > array.length
+  array[-1] .insert(0, "and ")
+  
 end
-array
+array.join(", ")
 end
